e0b060b994a24609fe2c06e7ce5edb66185565fc
İrem Özben <56583531+iremozbn@users.noreply.github.com>
2023-03-02 17:42:55 +0300

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,14 +1,14 @@
 import cv2
 import sys
 import matplotlib.pyplot as plt
-img = plt.imread(r"C:\Users\iremo\OneDrive\Desktop\lessons\ai\MM.jpg")  
+img = plt.imread("MM.jpg")  #burada secilen resmi yukledik
 
-plt.imshow(img)
-yuz_cascade = cv2.CascadeClassifier(r"C:\Users\iremo\OneDrive\Desktop\lessons\ai\data\haarcascades\haarcascade_frontalface_alt.xml")
-gray=cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
-yuz = yuz_cascade.detectMultiScale(img, scaleFactor=1.1, minNeighbors=3)
+plt.imshow(img) #resmin yuklendigi kontrolu yapıldı
+yuz_cascade = cv2.CascadeClassifier("haarcascade_frontalface_alt.xml") #hazir paket ile yuzu tanimladik
+gray=cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) #resmi gri renge cevirdik ki bilgisayar okuyabilsin
+yuz = yuz_cascade.detectMultiScale(img, scaleFactor=1.1, minNeighbors=3) #belirtilen alanlarda yuz taraması yapıldı
 for (x,y,w,h) in yuz: 
     cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255 ), 2)
     roi_gray = gray[y:y+h, x:x+w] 
     roi_color = img[y:y+h, x:x+w] 
-print("[INFO] {0} Yüz Bulundu!".format(len(yuz)))   
+print("{0} Yüz Bulundu!".format(len(yuz)))   
